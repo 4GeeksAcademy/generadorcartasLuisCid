ca44e6b13611ff47094ab41e44857c1260e6314a
Luis cid pedrero <luiscidp1@gmail.com>
2025-03-16 17:14:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const palos = ["♠", "♦", "♥"];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+  const palo = palos[Math.floor(Math.random() * palos.length)];
 
+  
+  document.getElementById("card-value").textContent = valor;
+  document.getElementById("card-suit-top-left").textContent = palo;
+  document.getElementById("card-suit-bottom-right").textContent = palo;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  const color = (palo === "♦" || palo === "♥") ? "red" : "black";
+  document.getElementById("card-suit-top-left").style.color = color;
+  document.getElementById("card-suit-bottom-right").style.color = color;
+}
